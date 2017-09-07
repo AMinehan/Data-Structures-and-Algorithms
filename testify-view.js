1377542d831e22a8ac1977ccf686580a967629e3
AMinehan <soloin@gmail.com>
2017-09-06 18:04:05 -0700

    splits testify into view and tests

--- testify-view.js
+++ testify-view.js
@@ -1,8 +1,12 @@
+// View section
+
 let rotateStart;
 
 let cube = findFirstElement(function(current){
   return current.className === 'cube';
 });
+
+//
 
 cube.style.transform = "transform: rotateX(0deg) rotateY(0deg);";
 
@@ -43,15 +47,21 @@ function findAllElements(func){
   return result;
 }
 
+//  Here are two ways to add event listeners: defining the property of an object and using the
+//  addEventListener function.  removeEventListener needs a reference to the function it's removing,
+//  so we're storing that reference in rotateStart.
+
 document.documentElement.onmousedown = function(event){
   rotateStart = mouseMove(event);
   document.documentElement.addEventListener('mousemove', rotateStart);
 }
 
 document.documentElement.onmouseup = function(event){
-  console.log(cube.style.transform)
   document.documentElement.removeEventListener('mousemove', rotateStart);
 }
+
+//  This is an example of closure.  The first time this function is called, it returns another function,
+//  which retains access to the variables contained inside the parent function.
 
 function mouseMove(startEvent){
   let last = 0;
@@ -60,34 +70,41 @@ function mouseMove(startEvent){
 
   let startStyle = cube.style.transform.match(/-?[0-9]+/g) || [0, 0];
 
-
   startStyle = startStyle.map(Number).reverse();
-
-
-  console.log(startStyle);
 
   let offsetX = startStyle[0];
   let offsetY = startStyle[1];
 
   return function(event){
-    console.log('mousemove')
     let current = Date.now();
+
+    // throttle function: limits calls to this function to once every 30 milliseconds, or 33.3 fps
     if ((current - last) <= 30){
       return;
     }
     last = current;
+
     let styles = styleStrings((offsetX + (event.x - startX)) % 360, (offsetY - (event.y - startY)) % 360);
-    cube.style = styles[0];
+
+    styles.forEach(function(c){
+      cube.style = c;
+    });
   }
 }
 
+//  If the variable orders here seem confusing, remember it's rotating around the X and Y axes, so we're passing
+//  in the opposite translate values.
+
+//  y
+//
+//  |
+//  |
+//  |
+//  +----- x
+
 function styleStrings(x, y){
   return ["-webkit-transform: rotateX(" + y + "deg) rotateY(" + x + "deg);",
-          "-moz-transform: rotateX(" + y + "deg) rotateY(" + x + "deg) translateZ(-600px);",
-          "-o-transform: rotateX(" + y + "deg) rotateY(" + x + "deg) translateZ(-600px);",
-          "transform: rotateX(" + y + "deg) rotateY(" + x + "deg) translateZ(-600px);"];
-}
-
-function test(object, func, result){
-  return JSON.stringify(func(object)) === JSON.stringify(result);
+          "-moz-transform: rotateX(" + y + "deg) rotateY(" + x + "deg);",
+          "-o-transform: rotateX(" + y + "deg) rotateY(" + x + "deg);",
+          "transform: rotateX(" + y + "deg) rotateY(" + x + "deg);"];
 }
