ed5282a85ce586cafa8d9ee8a5943f92fa366990
AMinehan <soloin@gmail.com>
2017-09-13 22:03:43 -0700

    finishes array descriptions

--- dataStructures/arrays.js
+++ dataStructures/arrays.js
@@ -1,14 +1,15 @@
 /*
     Arrays:
 
-  Arrays are one of the basic data structures to understand.  Interestingly, JavaScript originally shipped
-  without them, so now there's a mix of Arrays and array-like objects, like HTMLCollection and the arguments
-  object, which contain incrementally numbered properties and a length property.  The crucial difference between
-  them is that array-like objects do not inherit functions from the Array prototype, so trying to call forEach
-  on the arguments object will throw an error, since forEach will be undefined.
+  Arrays are one of the basic data structures to understand.  Interestingly,
+  JavaScript originally shipped without them, so now there's a mix of Array
+  objects and array-like objects, like HTMLCollection and the arguments object,
+  which contain incrementally numbered properties and a length property, but
+  not the functions attached to Array.prototype.
 
-  For this exercise, we will be re-creating some functions from the Array prototype to better understand how
-  they work.
+  For this exercise, we will be re-creating some functions from the Array
+  prototype to better understand how they work and when you might not want
+  to use them.
 
 */
 
@@ -17,10 +18,10 @@ const NewArray = function(){
 }
 
 /*
-  Push takes values and adds them to the end of an array.
+  Push takes a value and adds it to the end of an array.
 */
 
-NewArray.prototype.push = function(...values){
+NewArray.prototype.push = function(value){
   //FILL ME IN
 }
 
@@ -33,18 +34,21 @@ NewArray.prototype.pop = function(){
 }
 
 /*
-  Unshift adds a value to the front of an array.  What happens when there's a value already there?  Note
-  how much work the computer has to do as the size of the array increases.
+  Unshift adds a value to the front of an array.  What happens when there's
+  a value already there?  Note how much work the computer has to do as the
+  size of the array increases.
 
-  Advanced: use the spread operator (unshift = function(...values){}) to allow the function to take more than one value
+  bonus work: use the spread operator (unshift = function(...values){}) to
+  allow the function to take more than one value
 */
 
-NewArray.prototype.unshift = function(values){
+NewArray.prototype.unshift = function(value){
   //FILL ME IN
 }
 
 /*
-  Shift removes the value at the front of an array and returns it.  This is just as much work as unshift!
+  Shift removes the value at the front of an array and returns it.  This
+  is just as much work as unshift!
 */
 
 NewArray.prototype.shift = function(){
@@ -52,8 +56,9 @@ NewArray.prototype.shift = function(){
 }
 
 /*
-  slice takes an inclusive start index and an exclusive end index and returns a new array containing the elements contained
-  within that range.  This is also useful for copying an array.
+  slice takes an inclusive start index and an exclusive end index and returns
+  a new array containing the elements contained within that range.  Calling
+  slice without any arguments is also useful for copying an array.
 */
 
 NewArray.prototype.slice = function(start, end){
@@ -61,8 +66,9 @@ NewArray.prototype.slice = function(start, end){
 }
 
 /*
-  Splice takes a start index, a number of items to remove, and a number of optional values to replace them with.  It then
-  modifies the array in-place and returns the elements that have been removed.  For instance, if you called:
+  Splice takes a start index, a number of items to remove, and a number of
+  optional values to replace them with.  It then modifies the array in-place
+  and returns the elements that have been removed.  For instance, if you called:
 
   let foo = [1, 2, 3, 4, 5];
   foo.splice(1, 2, 6, 7, 8);
